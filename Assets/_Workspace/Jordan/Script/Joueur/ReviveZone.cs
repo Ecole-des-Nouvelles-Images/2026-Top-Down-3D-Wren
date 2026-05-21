ec491b7494199d808022a994b93582a5593abd52
Drayzzer <j.naval@nouvellesimages.xyz>
2026-05-21 16:43:20 +0200

    added wave and revive half finish

--- Assets/_Workspace/Jordan/Script/Joueur/ReviveZone.cs
+++ Assets/_Workspace/Jordan/Script/Joueur/ReviveZone.cs
@@ -1,29 +1,14 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Workspace.Jordan.Script.Joueur
 {
     public class ReviveZone : MonoBehaviour
     {
-        List<PlayerController> playersInZone = new List<PlayerController>();
-        private void OnTriggerEnter(Collider other)
-        {
-                if(other.CompareTag("Player"))
-                {
-                    PlayerController player = other.GetComponent<PlayerController>();
+        [SerializeField] private PlayerController _player;
 
-                    playersInZone.Add(player);
-                }
-        }
-        
-        private void OnTriggerExit(Collider other)
+        public void RevivePlayer()
         {
-            if(other.CompareTag("Player"))
-            {
-                PlayerController player = other.GetComponent<PlayerController>();
-
-                playersInZone.Remove(player);
-            }
+            _player.Revive();
         }
     }
 }
